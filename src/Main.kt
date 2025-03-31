04be876cacbf9b523fd6e42fc2d461e23b798bb0
vovabullet <vovurlav@mail.ru>
2025-03-31 19:21:12 +0300

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,90 @@
-// Задача 1
-fun rounderUp(num : Int ) : String {
-    if (num < 1000) {
-        return num.toString()
-    }
-    return (num/1000).toString() + "K"
-}
-
-// Задача 2
-fun yearsEnding (year : Int) : String {
-    val lastTwo = year % 100
-    val lastOne = year % 10
-    return when {
-        lastTwo in 11..14 -> "$year лет"
-        lastOne == 1 -> "$year год"
-        lastOne in 2..4 -> "$year года"
-        else -> "$year лет"
-    }
-}
-
 fun main() {
     // Задача 1
-    println(rounderUp(1234))
-    println(rounderUp(123))
-    println(rounderUp(12345))
+    val remains = 20 % 17
+    if (remains > 0) {
+        println("Получилось значение большее чем ноль!")
+    }
 
     // Задача 2
-    println(yearsEnding(12))
-    println(yearsEnding(1992))
-    println(yearsEnding(997))
+    println("Возраст:")
+    val number = readLine()?.toInt() ?: 0
+    val result = if (number > 18) {
+        "Вам уже все можно"
+    } else if (number == 18) {
+        "Ура, Вам 18 лет!"
+    } else {
+        "Вы еще слишком молоды"
+    }
+    println(result)
+
+    // Задание 3
+    println("У вас есть числа: 2 и 6\n1. Добавить\n2. Отнять\n0. Ничего")
+    val choose = readLine()?.toInt() ?: 0
+    println(
+        when (choose) {
+            1 -> 2+6
+            2 -> 2-6
+            0 -> return
+            else -> "Неверный выбор"
+        }
+    )
+
+    // Задание 4
+    print("Введите число от 1 до 7: ")
+    val input = readLine()?.toIntOrNull() ?: 0
+    println(
+        when (input) {
+            1 -> "Понедельник"
+            2 -> "Вторник"
+            3 -> "Среда"
+            4 -> "Четверг"
+            5 -> "Пятница"
+            6 -> "Суббота"
+            7 -> "Воскресенье"
+            else -> "Ошибка: Введите число от 1 до 7!"
+        }
+    )
+
+    // Задание 5
+    print("Введите первое число: ")
+    val a = readLine()?.toIntOrNull() ?: 0
+    print("Введите второе число: ")
+    val b = readLine()?.toIntOrNull() ?: 0
+    print("Введите третье число: ")
+    val c = readLine()?.toIntOrNull() ?: 0
+
+    val min = minOf(a, b, c)
+    val max = maxOf(a, b, c)
+
+    if (min == max) {
+        println("Ошибка")
+    } else {
+        val average = a + b + c - min - max
+        println("Среднее число: $average")
+    }
+
+    // Задание 6
+    print("Введите экспорт: ")
+    val export = readLine()?.toIntOrNull() ?: 0
+    print("Введите импорт: ")
+    val import = readLine()?.toIntOrNull() ?: 0
+
+    val balance = export - import
+
+    when {
+        balance > 0 -> println("Ваша прибыль составила: $balance")
+        balance < 0 -> println("Ваши убытки составили: ${balance*-1}")
+        else -> println("Сальдо нулевое")
+    }
+
+    // Задание 7
+    print("Введите первое число: ")
+    val f = readLine()?.toIntOrNull() ?: 0
+    print("Введите второе число: ")
+    val s = readLine()?.toIntOrNull() ?: 0
+
+    val result2 = if (f % 2 == 0 && s % 2 == 0) true else false
+    println(result2)
+
+
 }
